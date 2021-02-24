474fbba6f7a868e31f9687eaf2c2fdfa9fcd7498
MrAssassin23 <dharaiyaparthikr@gmail.com>
2021-02-24 19:29:29 +0530

    Info section

--- js/app.js
+++ js/app.js
@@ -14,26 +14,58 @@ $('#menubar-toogle').click(e => {
 
 $('div.expand').click(() => {
     let height = $('div.services-container').height()
-    if(height === 0){    
+    if (height === 0) {
         $('div.services-container').css({ 'height': 'auto' })
         $('img.expand-arrow').css({ 'transform': 'rotate(270deg)' })
         $('h3.expand').text('Weniger lesen')
-    }else{
+    } else {
         $('div.services-container').css({ 'height': '0' })
         $('img.expand-arrow').css({ 'transform': 'rotate(90deg)' })
         $('h3.expand').text('Mehr lesen')
     }
 })
 
+$('div.expand-portal').click(() => {
+    let height = $('div.point-expand').height()
+    if (height === 0) {
+        $('div.point-expand').css({'height':'auto' })
+        $('div.expand-portal img.expand-arrow').css({ 'transform': 'rotate(270deg)' })
+        $('div.expand-portal h3.expand').text('Weniger lesen')
+    } else {
+        $('div.point-expand').css({ 'height': '0' })
+        $('div.expand-portal img.expand-arrow').css({ 'transform': 'rotate(90deg)' })
+        $('div.expand-portal h3.expand').text('Mehr lesen')
+    }
+})
+
+$('div.expand-about').click(() => {
+    let height = $('div.about-right-expander').height()
+
+    if (height === 117) {
+        $('div.about-right-expander').css({'height':'auto' })
+        $('div.expand-about img.expand-arrow').css({ 'transform': 'rotate(270deg)' })
+        $('div.expand-about h3.expand').text('Weniger lesen')
+    } else {
+        $('div.about-right-expander').css({ 'height': '7.3rem' })
+        $('div.expand-about img.expand-arrow').css({ 'transform': 'rotate(90deg)' })
+        $('div.expand-about h3.expand').text('Mehr lesen')
+    }
+})
+
+createCarousel('carousel', 'slides-container', 2)
+
 function doCarousel() {
     createCarousel('benifit_carousel__container', 'benifit-container', 4)
+    createCarousel( 'about-slide-container','about-left',3 )
 }
 
 function stopCarousel() {
     $('.carousel-container').removeClass('carousel-container')
     let div_parent = $('.carousel-slide-container').removeClass('carousel-slide-container')
-    div_parent.css({ 'width': '100%' })
+    div_parent.css({ 'width': '100%','transform':'translateX(0%)' })
     $('.indicator-container').remove()
+    createCarousel('carousel', 'slides-container', 2)
+
 }
 
 if ($(window).width() < 800) {
@@ -42,6 +74,8 @@ if ($(window).width() < 800) {
 $(window).resize(function () {
     if ($(window).width() < 800) {
         doCarousel()
+        $('div.services-container').css({ 'height': '0', 'overflow': 'hidden' })
+
     }
     if ($(window).width() > 800) {
         stopCarousel()
@@ -61,7 +95,7 @@ function createCarousel(parentContainer, parent, childs) {
     // console.log({ div_parentContainer, div_parent })
 
     //create indicators
-    let indicators = createIndicators(4)
+    let indicators = createIndicators(childs)
     let indicator_childs = indicators.children
     for (let i = 0; i < childs; i++) {
         indicator_childs[i].addEventListener('click', e => moveCarousel(i))
